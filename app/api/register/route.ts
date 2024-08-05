1a27d63f2891abb96f7bc39d317d5037e4c5e32d
Dennis Muvaa <dmuvaa70@gmail.com>
2024-08-05 18:32:14 +0300

    Update Endpoints

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,24 +1,24 @@
-// // src/app/api/register/route.ts
-// import { NextRequest, NextResponse } from 'next/server';
-// import bcrypt from 'bcryptjs';
-// import { prisma } from './../../lib/prisma';
+// src/app/api/register/route.ts
+import { NextRequest, NextResponse } from 'next/server';
+import bcrypt from 'bcryptjs';
+import { prisma } from './../../lib/prisma';
 
-// export async function POST(req: NextRequest) {
-//   const { email, password, firstName, lastName } = await req.json();
+export async function POST(req: NextRequest) {
+  const { email, password, firstName, lastName } = await req.json();
 
-//   try {
-//     const hashedPassword = await bcrypt.hash(password, 10);
-//     await prisma.user.create({
-//       data: {
-//         email,
-//         password: hashedPassword,
-//         firstName,
-//         lastName,
-//       },
-//     });
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    await prisma.user.create({
+      data: {
+        email,
+        password: hashedPassword,
+        firstName,
+        lastName,
+      },
+    });
 
-//     return NextResponse.json({ message: 'User registered successfully' }, { status: 201 });
-//   } catch (error) {
-//     return NextResponse.json({ error: error.message }, { status: 500 });
-//   }
-// }
+    return NextResponse.json({ message: 'User registered successfully' }, { status: 201 });
+  } catch (error) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
